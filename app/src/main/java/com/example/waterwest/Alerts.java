05e0a9b22ee92799b7e683e4aaadd647534b9c3e
malaktatour <109721832+malaktatour@users.noreply.github.com>
2022-07-28 16:53:47 +0300

    Add Btn

---
+++ app/src/main/java/com/example/waterwest/Alerts.java
@@ -0,0 +1,4 @@
+package com.example.waterwest;
+
+public class Alerts {
+}
--- app/src/main/java/com/example/waterwest/MainActivity.java
+++ app/src/main/java/com/example/waterwest/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.waterwest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button LitersBtn, AlertBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LitersBtn = findViewById(R.id.LitersBtn);
+
+        LitersBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),WaterUsage.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
