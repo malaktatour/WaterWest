0889e0f5ca0e301ccf523b698e41615292339607
malaktatour <109721832+malaktatour@users.noreply.github.com>
2022-07-28 17:11:56 +0300

    error handling

--- app/src/main/java/com/example/waterwest/WaterUsage.java
+++ app/src/main/java/com/example/waterwest/WaterUsage.java
@@ -1,4 +1,19 @@
 package com.example.waterwest;
 
-public class WaterUsage {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WaterUsage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.water_usage);
+
+
+    }
+
 }
